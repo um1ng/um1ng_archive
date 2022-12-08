98c073e2b7fae32a880ab716b22d93cd518dbc28
um1ng <ikashue@gmail.com>
2022-12-08 14:31:37 +0800

    update

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -4,7 +4,7 @@ async function config() {
   const pageSize = 3;
   await generatePaginationPages(pageSize);
   return {
-    title: "um1ng",
+    title: "🎉",
     base: "/",
     description: "um1ng blog",
     ignoreDeadLinks: true,
@@ -12,11 +12,9 @@ async function config() {
       posts: await getPosts(),
       pageSize: pageSize,
       nav: [
-        { text: "Home", link: "/" },
         { text: "Archives", link: "/pages/archives" },
         { text: "Tags", link: "/pages/tags" },
         { text: "About", link: "/pages/about" },
-        // { text: 'Airene', link: 'http://airene.net' }  -- External link test
       ],
       //outline:[2,3],
       outlineTitle: "文章摘要",
@@ -30,14 +28,9 @@ async function config() {
     srcExclude: ["README.md"], // exclude the README.md , needn't to compiler
 
     vite: {
-      //build: { minify: false }
-      server: { port: 5000 },
+      build: { minify: false },
+      server: { port: 9999 },
     },
-    /*
-      optimizeDeps: {
-          keepNames: true
-      }
-      */
   };
 }
 
